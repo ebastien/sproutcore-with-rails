397970e08a6fdd04d506dba9e1e24d94f275ae20
Emmanuel Bastien <bastien_emmanuel@yahoo.fr>
2011-07-13 23:46:59 +0200

    Prepare HTML specs and features.

---
+++ features/html/step_definitions/html_steps.rb
@@ -0,0 +1,15 @@
+Given /^I am on the home page$/ do
+  visit('/home')
+end
+
+When /^I login as "([^"]*)" with password "([^"]*)"$/ do |login, password|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^I am authenticated$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^I get my account$/ do
+  pending # express the regexp above with the code you wish you had
+end
--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -7,8 +7,20 @@ describe HomeController do
         api_get
         response.content_type.should == API_MIME
         response.status.should == 200
+        # TODO: Maybe we should only check the behavior (i.e JSON rendering) and not the outcome
         decode_media! response.body
         link('login').should be_true
+      end
+    end
+  end
+  
+  describe "HTML" do
+    describe "GET home page" do
+      it "answers with the correct template" do
+        get :show
+        response.content_type.should == Mime::HTML
+        response.status.should == 200
+        response.should render_template('show')
       end
     end
   end
