657082f534fe282dc2bee7394f69d146066c3d4d
Emmanuel Bastien <os@ebastien.name>
2011-08-08 00:03:33 +0200

    Test going through the login API from the SC app.

--- app/assets/javascripts/ui/welcome.js
+++ app/assets/javascripts/ui/welcome.js
@@ -1,18 +1,39 @@
-$.ajaxSetup({
-  cache: false,
-  dataType: 'json',
-  accepts: { json: "application/vnd.dsim+json;version=1" }
+
+SCApp = SC.Application.create({
+  API_MIME: "application/vnd.dsim+json;version=1",
+  API_ROOT: "/home"
 });
 
-SCApp = SC.Application.create({});
+SCApp.searchLink = function(res, rel) {
+  if (res.links && res.links.length) {
+    for (var i=0; i<res.links.length; i++) {
+      if (res.links[i]["rel"] === rel) {
+        return res.links[i]["href"];
+      }
+    }
+  }
+  return;
+}
+
+SCApp.testLogin = function() {
+  $.ajax({ url: SCApp.API_ROOT }).done(
+    function(res) {
+      var login_url = SCApp.searchLink(res, "login");
+      if (login_url) {
+        var payload = JSON.stringify({ login: "John", password: "secret" });
+        $.ajax({ url: login_url, type: "POST", data: payload }).done(
+          function(res) {
+            console.log(res);
+          }
+        );
+      }
+    }
+  );
+}
 
 SCApp.welcomeController = SC.Object.create({
   takeOverTheWorld: function() {
-    $.ajax({ url: '/home' }).done(
-      function(response) {
-        // Do something interesting
-      }
-    );
+    SCApp.testLogin();
   }
 });
 
@@ -21,6 +42,15 @@ SCApp.WelcomeView = SC.View.extend({
 });
 
 SC.$(document).ready(function() {
+  
+  $.ajaxSetup({
+    cache: false,
+    dataType: 'json',
+    accepts: { json: SCApp.API_MIME },
+    contentType: SCApp.API_MIME,
+    processData: false
+  });
+  
   SCApp.mainView = SCApp.WelcomeView.create();
-  SCApp.mainView.append();
+  SCApp.mainView.append();  
 });
